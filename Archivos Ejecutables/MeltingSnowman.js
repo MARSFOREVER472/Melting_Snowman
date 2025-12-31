c796c49cbd5ddea29c6f7d4515d3ca18b2254288
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-12-31 15:50:21 -0300

    Agregando una función al archivo...

--- Archivos Ejecutables/MeltingSnowman.js
+++ Archivos Ejecutables/MeltingSnowman.js
@@ -2,3 +2,9 @@
 
 const words = ['DANIEL', 'JADUE', 'PARTIDO', 'COMUNISTA', 'CHILENO', 'ACCION', 'PROLETARIA'];
 
+// FUNCIÓN A DEFINIR EN ESTE APARTADO...
+
+function elegirPalabraAleatoria()
+{
+    return words[Math.floor(Math.random() * words.length)];
+}
